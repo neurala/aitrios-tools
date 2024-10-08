4519ea2edc2302974108cc8245a04566abb0b190
JR Raynal <jr@neurala.com>
2024-10-08 16:02:50 -0400

    Fixed typos in code

--- execute/execute.py
+++ execute/execute.py
@@ -16,23 +16,21 @@ parser.add_argument(
     "--aitrios_secrets",
     "-s",
     type=str,
-    dest="aitrios_secrets",
-    help=f"The path to your AITIRIOS console configuration file",
+    help="The path to your AITIRIOS console configuration file",
     required=True,
 )
 parser.add_argument(
     "--device_name",
     "-D",
     type=str,
-    dest="device_name",
-    help=f"Device Name to search for",
+    help="Device Name to search for",
     required=True,
 )
 
 
-def load_configuration_file(configuration_path: Path, schema_path: Path):
+def load_configuration_file(configuration_path: Path, schema_path: Path) -> dict:
     """
-    Parse a secrects configuration file, validates against schema
+    Parse a secrets configuration file, validate against schema
     """
     # Load configuration file
     with open(configuration_path, "r") as f:
@@ -51,37 +49,38 @@ def create_client(aitrios_secrets):
     """
     Create a connection object
     """
-    json_load = load_configuration_file(
+    config = load_configuration_file(
         aitrios_secrets, "./json_schemas/console_configuration_schema.json"
     )
     return AitriosConsole(
-        json_load["console_endpoint"],
-        json_load["client_id"],
-        json_load["client_secret"],
-        json_load["portal_authorization_endpoint"],
+        config["console_endpoint"],
+        config["client_id"],
+        config["client_secret"],
+        config["portal_authorization_endpoint"],
     )
 
 
 def jsonify(data):
     """
-    Dumnp data as a JSON element, pretty formated
+    Dump data as a JSON element, pretty formated
     """
     return json.dumps(data, indent=4)
 
 if __name__ == "__main__":
     print("Initiating Console connection")
     args = parser.parse_args()
-    client_obj = create_client(args.aitrios_secrets)
+    client = create_client(args.aitrios_secrets)
 
     # 1. Get a device information
     print(f"Looking for device {args.device_name}")
-    request = client_obj.GetDevices(
-        connectionState="connected", device_name=args.device_name
+    request = client.GetDevices(
+        connectionState="connected",
+        device_name=args.device_name
     )
     devices = request["devices"]
-    if not devices or len(devices) == 0:
-        connected_devices = client_obj.GetDevices(connectionState="connected")
-        print(f"Connected Devices")
+    if not devices:
+        connected_devices = client.GetDevices(connectionState="connected")
+        print("Connected Devices")
         for device in connected_devices["devices"]:
             name = device["property"]["device_name"]
             dev_id = device["device_id"]
@@ -92,7 +91,7 @@ if __name__ == "__main__":
     device_id = devices[0]["device_id"]
 
     print("Retrieving CommandParameters")
-    response = client_obj.GetCommandParameterFile()
+    response = client.GetCommandParameterFile()
     for command_parameter in response["parameter_list"]:
         if device_id in command_parameter["device_ids"]:
             print(jsonify(command_parameter))
@@ -100,7 +99,7 @@ if __name__ == "__main__":
 
     # 2. Turn logs on
     print("Activating logs")
-    response = client_obj.Request(
+    response = client.Request(
         url="/devices/{device_id}/configuration/applog",
         method="PUT",
         device_id=device_id,
@@ -109,7 +108,7 @@ if __name__ == "__main__":
     print(jsonify(response))
 
     print("Setting device destination")
-    response = client_obj.Request(
+    response = client.Request(
         url="/devices/{device_id}/configuration/logdestination",
         method="PUT",
         device_id=device_id,
@@ -119,36 +118,41 @@ if __name__ == "__main__":
 
     # 3. Start processing
     print("Starting processing")
-    response = client_obj.StartUploadInferenceResult(device_id=device_id)
+    response = client.StartUploadInferenceResult(device_id=device_id)
     print(jsonify(response))
-    if not "SUCCES" in response["result"]:
+    if not "SUCCESS" in response["result"]:
         exit(2)
     output_subdirectory = response["outputSubDirectory"]
 
-    # Wait for processing do do something useful
+    # Wait for processing to do something useful
+    print("Waiting on camera to infer...")
     time.sleep(10)
 
     # 4. Downloads Results
     print("Getting results")
-    # while not result:
-    result = client_obj.GetInferenceResults(
+    result = client.GetInferenceResults(
         device_id=device_id, NumberOfInferenceresults=1, raw=1
     )
     print(jsonify(result))
 
     # 5. Download logs
     # sleeping to make sure all logs are loaded in the database
+    print("Waiting on camera to gather logs...")
     time.sleep(10)
-    print("Downloading logs")
+    topNLogs = 50
+    print(f"Downloading last {topNLogs} logs")
 
     # Hardcoding "top" here to return the N latest logs
-    response = client_obj.Request(
-        url="/devices/{device_id}/applogs?top=59", method="GET", device_id=device_id
+    response = client.Request(
+        url="/devices/{device_id}/applogs?top={topNLogs}",
+        method="GET",
+        device_id=device_id,
+        topNLogs=topNLogs
     )
     print(jsonify(response))
 
     # Turning logs off
-    response = client_obj.Request(
+    response = client.Request(
         url="/devices/{device_id}/configuration/applog",
         method="PUT",
         device_id=device_id,
@@ -158,5 +162,5 @@ if __name__ == "__main__":
 
     # 6. Stop processing
     print("Stopping processing")
-    response = client_obj.StopUploadInferenceResult(device_id=device_id)
+    response = client.StopUploadInferenceResult(device_id=device_id)
     print(jsonify(response))
